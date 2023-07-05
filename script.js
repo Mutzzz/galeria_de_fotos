7f767c5d6486a93904e96f26b770cfbc18a33868
Gálatas Mutz <galatasmutz@gmail.com>
2023-07-05 18:40:39 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    $('header button').click(function() {
+        $('form').slideDown();
+    })
+
+    $('#cancel-button').click(function(){
+        $('form').slideUp();
+    })
+
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+        const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
+        const novoItem = $('<li style="display: none"></li>')
+        $(`<img src="${enderecoDaNovaImagem}" />`).appendTo(novoItem);
+        $(`<div class="overlay-imagem-link">
+                <a href="${enderecoDaNovaImagem}" target="_blank" tittle="Ver imagem em tamanho real">
+                    Ver imagem em tamanho real
+                </a>
+            </div>
+        
+        `).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(1000);
+        $('#endereco-imagem-nova').val('')
+
+    })
+})
+
+//----------------------------------------------------------//
+
+
+
